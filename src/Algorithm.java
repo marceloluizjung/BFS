cceaa65b5f08e588463cc0b7a1459531c01145f9
Marcelo <marcelo_4940@hotmail.com>
2020-10-21 21:03:05 -0300

    Estrutura

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,4 +1,4 @@
-package PACKAGE_NAME;
-
 public class Algorithm {
+
+    public void BFS() {}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class Main {
+
+
+    public static void main(String[] args) {
+        String[] entrada = {
+                "B A",
+                "Entrada A",
+                "B GT",
+                "GT H",
+                "H *",
+                "B *",
+                "* C",
+                "C I",
+                "I D",
+                "C D",
+                "D Saida"
+        };
+    }
 }
