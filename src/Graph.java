f34937ef6586abef99ada5249b60647a9b1c5af6
Marcelo <marcelo_4940@hotmail.com>
2020-10-22 21:45:08 -0300

    BFS pronta

--- src/Graph.java
+++ src/Graph.java
@@ -1,49 +1,51 @@
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Graph {
     private int V;   // No. of vertices
-    private LinkedList<Integer> adj[]; //Adjacency Lists
+    private LinkedHashSet<String> verticesList = new LinkedHashSet<>();
 
-    // Constructor
-    Graph(int v) {
-        V = v;
-        adj = new LinkedList[v];
-        for (int i = 0; i < v; ++i)
-            adj[i] = new LinkedList();
-    }
-
-    // Function to add an edge into the graph
-    void addEdge(int v, int w) {
-        adj[v].add(w);
+    public void addVertices(List<String> vertices) {
+        this.verticesList.addAll(vertices);
     }
 
     // prints BFS traversal from a given source s
-    void BFS(int s) {
+    public void BFS(String s, String[] graph) {
         // Mark all the vertices as not visited(By default
         // set as false)
-        boolean visited[] = new boolean[V];
+        HashMap<String, Boolean> visited = new HashMap();
 
-        // Create a queue for BFS
-        LinkedList<Integer> queue = new LinkedList<Integer>();
+        // Fila BFS
+        LinkedList<String> queue = new LinkedList<String>();
 
-        // Mark the current node as visited and enqueue it
-        visited[s] = true;
+        // Marca o nó como visitado
+        visited.put(s, true);
         queue.add(s);
 
         while (queue.size() != 0) {
-            // Dequeue a vertex from queue and print it
+            // Remove o vétice da lista
             s = queue.poll();
             System.out.print(s + " ");
 
             // Get all adjacent vertices of the dequeued vertex s
             // If a adjacent has not been visited, then mark it
             // visited and enqueue it
-            Iterator<Integer> i = adj[s].listIterator();
+
+            //Catando os vértices adjacentes (é aqui que aplica a ação de escolher o próximo vértice a ser processado)
+            List<String> adjByS = new ArrayList<>();
+            for (int count = 0; count < graph.length; count++) {
+                String[] vertices = graph[count].split(" ");
+                if (vertices[0].equals(s)) {
+                    adjByS.add(vertices[1]);
+                } else if (vertices[1].equals(s)) {
+                    adjByS.add(vertices[0]);
+                }
+            }
+
+            Iterator<String> i = adjByS.listIterator();
             while (i.hasNext()) {
-                int n = i.next();
-                if (!visited[n]) {
-                    visited[n] = true;
+                String n = i.next();
+                if (visited.get(n) == null) {
+                    visited.put(n, true);
                     queue.add(n);
                 }
             }
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,29 @@
 // Java program to print BFS traversal from a given source vertex.
 // BFS(int s) traverses vertices reachable from s.
 
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
 // This class represents a directed graph using adjacency list
 // representation
 //https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
 class Main {
     // Driver method to
-    public static void main(String args[]) {
-        Graph g = new Graph(4);
 
-        g.addEdge(0, 1);
-        g.addEdge(0, 2);
-        g.addEdge(1, 2);
-        g.addEdge(2, 0);
-        g.addEdge(2, 3);
-        g.addEdge(3, 3);
+    public static void main(String args[]) {
+        String[] graph = {
+                "0 1",
+                "0 2",
+                "1 2",
+                "2 0",
+                "2 3",
+                "3 3"
+        };
+        Graph g = new Graph();
 
         System.out.println("Following is Breadth First Traversal " +
                 "(starting from vertex 2)");
-
-        g.BFS(2);
+        g.BFS("2", graph);
     }
 }
 // This code is contributed by Aakash Hasija
